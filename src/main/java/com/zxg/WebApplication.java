4061aff2ab70ef44d00fb2b14e5c0d3e92d08f02
zhangxiaoguang <zhangxiaoguang>
2019-10-16 10:31:23 +0800

    Initial commit

---
+++ src/main/java/com/zxg/WebApplication.java
@@ -0,0 +1,15 @@
+package com.zxg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxiaoguang
+ * @createtime 2019/10/16-10:18
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
---
+++ src/main/java/com/zxg/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.zxg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangxiaoguang
+ * @createtime 2019/10/16-10:25
+ */
+@RestController
+public class UserController {
+    @RequestMapping("/getUser")
+    public String getUser(){
+        return "user is student";
+    }
+}
