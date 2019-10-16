8ee658da55fe70115e670e3f27e7ef98306c3667
zhangxiaoguang <zhangxiaoguang>
2019-10-16 11:08:52 +0800

    @Value读取配置文件的配置

--- src/main/java/com/zxg/controller/UserController.java
+++ src/main/java/com/zxg/controller/UserController.java
@@ -3,12 +3,15 @@ package com.zxg.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
 /**
  * @author zhangxiaoguang
  * @createtime 2019/10/16-10:25
  */
 @RestController
 public class UserController {
+
     @RequestMapping("/getUser")
     public String getUser(){
         return "user is student";
---
+++ src/main/java/com/zxg/controller/config/JdbcConfig.java
@@ -0,0 +1,36 @@
+package com.zxg.controller.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zhangxiaoguang
+ * @createtime 2019/10/16-10:35
+ */
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class JdbcConfig {
+    @Value("${jdbc.driverClassName}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource druidDataSource=new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
+    }
+
+}
